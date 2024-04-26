b1186fbe2f92a48011b5d4625dad12fb6e468031
Jason Hardman <jason.hardman@churchofjesuschrist.org>
2024-04-26 16:51:34 -0600

    added review for lesson 1, introduction to JavaScript

---
+++ I-Introduction_to_javaScript/review/app.js
@@ -0,0 +1,24 @@
+console.log('This is printed output');
+
+// Single line comment
+
+/*
+Multi-line comment
+*/
+
+console.log('this text is printed' /*but this text is not */);
+
+console.log('single or double quote');  // String
+console.log(-123445.898769634);  // Number
+console.log(true);  // Boolean
+console.log(null);  // Null
+console.log(undefined);  // Undefined
+console.log('¯\\(ツ)/¯');  // Sybols
+console.log('<A collection of related data>');  // Objects
+
+console.log(1 + 2 * 3 % 2 -1);  // Builtin arithmetic operators
+
+console.log('length'.length);  // the length of the string 'length' (number of letters)
+console.log('meth'.toUpperCase() + 'ods');  // Methods and concatenation
+
+console.log(Math.ceil(14.0000001));  // Math methods - this one returns the next whole integer
