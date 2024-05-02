60defff13bc39cf24704cad71e9f47f7d719a8dd
Jason Hardman <jason.hardman@churchofjesuschrist.org>
2024-05-02 14:28:30 -0600

    added 1.2 review script

---
+++ I.2-Introduction_to_Variables/review/main.js
@@ -0,0 +1,11 @@
+let myAge = 38;
+let myName = 'Jason Hardman';
+console.log(`My name is ${myName} and I'm ${myAge} years old.`);
+console.log('So, at age ' + myAge + ', I\'m feeling my age more and more.');
+let deathDate = 'unknown';
+console.log(deathDate);
+let label = ' years old';
+console.log(typeof myAge);
+console.log(typeof label);
+let ageWithLabel = myAge + label;
+console.log(typeof ageWithLabel);
