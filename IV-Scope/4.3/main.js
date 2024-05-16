98162b25bd139740b7f72235bc1d5e1f4f4dd6c2
Jason Hardman <jason.hardman@churchofjesuschrist.org>
2024-05-16 16:26:44 -0600

    Global Scope script for 4.3

---
+++ IV-Scope/4.3/main.js
@@ -0,0 +1,8 @@
+const satellite = 'The Moon';
+const galaxy = 'The Milky Way';
+const stars = 'North Star';
+
+//const callMyNightSky = () => 'Night Sky: ' + satellite + ', ' + galaxy + ', and ' + stars;
+const callMyNightSky = () => `Night Sky: ${satellite}, ${stars}, and ${galaxy}`;
+
+console.log(callMyNightSky());
