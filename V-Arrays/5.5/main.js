a9ed5d2d9543c16a1a2aab7e57e83b0c9728ab91
Jason Hardman <jason.hardman@churchofjesuschrist.org>
2024-05-17 10:44:40 -0600

    Arrays with let and const script added - 5.5

---
+++ V-Arrays/5.5/main.js
@@ -0,0 +1,19 @@
+let condiments = ['Ketchup', 'Mustard', 'Soy Sauce', 'Sriracha'];
+
+const utensils = ['Fork', 'Knife', 'Chopsticks', 'Spork'];
+
+condiments[0] = 'Mayo';
+
+console.log(condiments);
+
+condiments = [condiments[0]];
+
+console.log(condiments);
+
+utensils[3] = 'Spoon';
+
+console.log(utensils);
+
+utensils = [utensils[3]];
+
+console.log(utensils);
