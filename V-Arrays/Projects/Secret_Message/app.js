bc5c61506f2576a44fe79ba1d5a9c292c39f6ce8
Jason Hardman <jason.hardman@churchofjesuschrist.org>
2024-05-20 13:10:25 -0600

    array manipulation project secret message

--- V-Arrays/Projects/Secret_Message/app.js
+++ V-Arrays/Projects/Secret_Message/app.js
@@ -1,10 +1,11 @@
+// Starting message
 let secretMessage = ['Learning', 'is', 'not', 'about', 'what', 'you', 'get', 'easily', 'the', 'first', 'time,', 'it', 'is', 'about', 'what', 'you', 'can', 'figure', 'out.', '-2015,', 'Chris', 'Pine,', 'Learn', 'JavaScript'];
 
 // Use an array method to remove the last string of the array `secretMessage`.
-console.log(secretMessage.length);
+//JH console.log(secretMessage.length);
 secretMessage.pop();
 // Great! You can check your work by logging the .length of the array.
-console.log(secretMessage.length);
+//JH console.log(secretMessage.length);
 
 // Use an array method to add the words `to` and `Program` as separate 
 // strings to the end of the secretMessage array.
@@ -12,7 +13,7 @@ secretMessage.push('to', 'Program');
 
 // Change the word `easily` to the word `right` by accessing the index 
 // and replacing it.
-secretMessage[secretMessage.indexOf('easily')] = 'right';
+secretMessage.splice(secretMessage.indexOf('easily'), 1, 'right');
 
 // Use an array method to remove the first string of the array.
 secretMessage.shift();
@@ -23,5 +24,9 @@ secretMessage.unshift('Programming');
 
 // Use an array method to remove the strings `get`, `right`, `the`, 
 // `first`, `time,`,, and replace them with the single string `know,`.
-console.log(secretMessage.flatMap((phrase) => phrase === ['get', 'right', 'the', 'first', 'time,'] ? 'know,'));
-console.log(secretMessage);
+const toBeReplaced = ['get', 'right', 'the', 'first', 'time,'];
+const toReplace = 'know,';
+secretMessage.splice(secretMessage.indexOf(toBeReplaced[0]), toBeReplaced.length, toReplace);
+
+// On one line, use `console.log()` and `.join()` to print the secret message as a sentence.
+console.log(secretMessage.join(' '));
