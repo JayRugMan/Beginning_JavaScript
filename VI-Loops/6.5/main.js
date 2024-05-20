0db1df175402a1ece4eeebf3359fc68f52144bc0
Jason Hardman <jason.hardman@churchofjesuschrist.org>
2024-05-20 13:55:06 -0600

    Looping through Arrays script added - 6.5

---
+++ VI-Loops/6.5/main.js
@@ -0,0 +1,7 @@
+const vacationSpots = ['Machu Picchu', 'The Coasts of Ireland', 'Okinawa'];
+//JH const vacationSpots = ['Bali', 'Paris', 'Tulum'];
+
+// Write your code below
+for (let i = 0; i < vacationSpots.length; i++) {
+  console.log(`I'd love to visit ${vacationSpots[i]}!`);
+}
