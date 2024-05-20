0fc3b9df1554593473bb6fced8e62a189ad4e3cf
Jason Hardman <jason.hardman@churchofjesuschrist.org>
2024-05-20 13:31:49 -0600

    Wetting my pallete on loops - 6.2

---
+++ VI-Loops/6.2/main.js
@@ -0,0 +1,8 @@
+// Write your code below
+
+const vacationSpots = ['Machu Picchu', 'The Coasts of Ireland', 'Okinawa']
+
+console.log(vacationSpots[0]);
+console.log(vacationSpots[1]);
+console.log(vacationSpots[2]);
+
